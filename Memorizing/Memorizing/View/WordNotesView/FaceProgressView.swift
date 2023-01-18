9ce6b4006d1a9a68f4ca474ac8049e3d4bd065e3
HyeonGi Yoon <yhg920@naver.com>
2023-01-18 13:21:39 +0900

    [feat]progressView 프로그래스바 애니메이션 작업중 중단

--- Memorizing/Memorizing/View/WordNotesView/FaceProgressView.swift
+++ Memorizing/Memorizing/View/WordNotesView/FaceProgressView.swift
@@ -10,103 +10,141 @@ import SwiftUI
 // MARK: 단어장 진행도 view
 struct FaceProgressView: View {
     
-    //    @Binding var faceArray: [String]
     var myWordNote: WordNote
-    
-    // 한 번도 안 하면 -1, 한 번씩 할 때마다 1씩 증가
-    //    @State var repeatCount: Int = 0
     
     var body: some View {
         ZStack {
+            
+            // MARK: ProgressBar baseLine
             Rectangle()
                 .frame(width: 260, height: 8)
                 .foregroundColor(.gray5)
             
+            // MARK: 진행도에 따라 Progressbar 길이 조절
             HStack {
                 Rectangle()
-                    .frame(width: myWordNote.progressbarWitdh, height: 8)
+                    .frame(width: myWordNote.progressbarWitdh,
+                           height: 8)
                     .foregroundColor(myWordNote.noteColor)
                     .padding(.leading, 25)
+                    .animation(.linear(duration: 1),
+                               value: myWordNote.progressbarWitdh)
+                
                 Spacer()
             }
             
             HStack(spacing: 55) {
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(myWordNote.repeatCount <= 0 ? .gray5 : myWordNote.noteColor)
-                    .overlay {
-                        if myWordNote.repeatCount <= 0 {
-                            Text("1")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                        } else {
-                            Image("FaceNomal")
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                        }
-                    }
                 
+                // MARK: 1회차
                 Circle()
                     .frame(width: 30, height: 30)
-                    .foregroundColor(myWordNote.repeatCount <= 1 ? .gray5 : myWordNote.noteColor)
+                    .foregroundColor(myWordNote.repeatCount <= 0
+                                     ? .gray5
+                                     : myWordNote.noteColor)
                     .overlay {
-                        if myWordNote.repeatCount <= 1 {
-                            Text("2")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                        } else {
-                            Image("CheckMark")
-                                .resizable()
-                                .frame(width: 24, height: 24)
+                        Text("1")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .overlay {
+                        if myWordNote.repeatCount > 0 {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(myWordNote.noteColor)
+                                // FIXME: DB수정 후 결과에 따라 표정 변하도록 수정
+                                Image("FaceNomal")
+                                    .resizable()
+                                    .frame(width: 24,
+                                           height: 24)
+                            }
                         }
                     }
+                    .animation(.easeInOut.delay(0.9),
+                               value: myWordNote.repeatCount)
                 
+                // MARK: 2회차
                 Circle()
                     .frame(width: 30, height: 30)
-                    .foregroundColor(myWordNote.repeatCount <= 2 ? .gray5 : myWordNote.noteColor)
+                    .foregroundColor(myWordNote.repeatCount <= 1
+                                     ? .gray5
+                                     : myWordNote.noteColor)
                     .overlay {
-                        if myWordNote.repeatCount <= 2 {
-                            Text("3")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                        } else {
-                            Image("CheckMark")
-                                .resizable()
-                                .frame(width: 24, height: 24)
+                        Text("2")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .overlay {
+                        if myWordNote.repeatCount > 1 {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(myWordNote.noteColor)
+                                Image("CheckMark")
+                                    .resizable()
+                                    .frame(width: 24,
+                                           height: 24)
+                            }
                         }
                     }
+                    .animation(.linear.delay(0.6),
+                               value: myWordNote.repeatCount)
                 
+                // MARK: 3회차
                 Circle()
                     .frame(width: 30, height: 30)
-                    .foregroundColor(myWordNote.repeatCount <= 3 ? .gray5 : myWordNote.noteColor)
+                    .foregroundColor(myWordNote.repeatCount <= 2
+                                     ? .gray5
+                                     : myWordNote.noteColor)
                     .overlay {
-                        if myWordNote.repeatCount <= 3 {
-                            Text("4")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                        } else {
-                            Image("FaceGood")
-                                .resizable()
-                                .frame(width: 24, height: 24)
+                        Text("3")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .overlay {
+                        if myWordNote.repeatCount > 2 {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(myWordNote.noteColor)
+                                Image("CheckMark")
+                                    .resizable()
+                                    .frame(width: 24,
+                                           height: 24)
+                            }
                         }
                     }
+                    .animation(.easeInOut.delay(0.6),
+                               value: myWordNote.repeatCount)
+                
+                // MARK: 4회차
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(myWordNote.repeatCount <= 3
+                                     ? .gray5
+                                     : myWordNote.noteColor)
+                    .overlay {
+                        Text("4")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .overlay {
+                        if myWordNote.repeatCount > 3 {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(myWordNote.noteColor)
+                                // FIXME: DB수정 후 결과에 따라 표정 변하도록 수정
+                                Image("FaceGood")
+                                    .resizable()
+                                    .frame(width: 24,
+                                           height: 24)
+                            }
+                        }
+                    }
+                    .animation(.easeInOut.delay(0.6),
+                               value: myWordNote.repeatCount)
             }
-            
-            // 테스트용 버튼
-            //            HStack(spacing: 55) {
-            //                Button("-1") {
-            //                    self.repeatCount -= 1
-            //                }
-            //
-            //                Text("\(self.repeatCount)")
-            //                    .font(.headline)
-            //
-            //                Button("+1") {
-            //                    self.repeatCount += 1
-            //                }
-            //            }
-            //            .padding(.top, 450)
-            
         }
     }
 }
@@ -117,7 +155,7 @@ struct FaceProgressView_Previews: PreviewProvider {
                                               noteName: "테스트노트",
                                               noteCategory: "영어",
                                               enrollmentUser: "hKWRjBcFi0NTuR5IjGHSsGMZUaV2",
-                                              repeatCount: 1,
+                                              repeatCount: 3,
                                               notePrice: 0)
                          //                         repeatCount: 0
         )
