a8bf086b0a2f5d874a042b36b6bd097fa2672c58
hyehyehye99 <hyehyehye99@naver.com>
2023-01-18 18:15:33 +0900

    dei

--- Memorizing/Memorizing/View/WordNotesView/WordNotesView.swift
+++ Memorizing/Memorizing/View/WordNotesView/WordNotesView.swift
@@ -35,14 +35,14 @@ struct WordNotesView: View {
                 Header(memoryStepToggle: $memoryStepToggle, reviewStepToggle: $reviewStepToggle, menuXAxis: $menuXAxis)
                 
                 if memoryStepToggle == true && reviewStepToggle == false {
-                    ScrollView {
+                    ScrollView(showsIndicators: false) {
                         ForEach(userStore.myWordNotes) { myWordNote in
                             WordRegistrationView(myWordNote: myWordNote)
                         }
                     }
                     
                 } else if memoryStepToggle == false && reviewStepToggle == true {
-                    ScrollView {
+                    ScrollView(showsIndicators: false) {
                         ForEach(userStore.myWordNotes) { myWordNote in
                             StudyAgainView(myWordNote: myWordNote)
                         }
@@ -69,26 +69,27 @@ struct WordNotesView: View {
             }
             VStack {
                 
-                Button {
-                    print("새로운 일기장 만들기 버튼이 눌렸습니다.")
-                    isShowingNewMemorySheet.toggle()
-                } label: {
-                    Circle()
-                        .foregroundColor(.mainBlue)
-                        .frame(width: 65, height: 65)
-                        .overlay {
-                            Image(systemName: "plus")
-                                .foregroundColor(.white)
-                                .font(.title3)
-                                .bold()
-                        }
-                        .shadow(radius: 1, x: 1, y: 1)
+                if memoryStepToggle == true && reviewStepToggle == false {
+                    Button {
+                        print("새로운 일기장 만들기 버튼이 눌렸습니다.")
+                        isShowingNewMemorySheet.toggle()
+                    } label: {
+                        Circle()
+                            .foregroundColor(.mainBlue)
+                            .frame(width: 65, height: 65)
+                            .overlay {
+                                Image(systemName: "plus")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                                    .bold()
+                            }
+                            .shadow(radius: 1, x: 1, y: 1)
+                    }
+                    .offset(x: 140, y: 250)
+                    .sheet(isPresented: $isShowingNewMemorySheet) {
+                        NewMakeMemoryNote(isShowingNewMemorySheet: $isShowingNewMemorySheet)
+                    }
                 }
-                .offset(x: 140, y: 250)
-                .sheet(isPresented: $isShowingNewMemorySheet) {
-                    NewMakeMemoryNote(isShowingNewMemorySheet: $isShowingNewMemorySheet)
-                }
-                
             }
             
             //            if(onboard.showOnboardScreen) {
