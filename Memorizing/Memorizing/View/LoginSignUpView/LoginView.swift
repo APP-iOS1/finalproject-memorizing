e41ec2e56b6d0b36e1d21fdb5581b369e2b2c334
JongHyunLee84 <whdgus3697@naver.com>
2023-01-20 15:11:25 +0900

    feat: asset black for darkMode

--- Memorizing/Memorizing/View/LoginSignUpView/LoginView.swift
+++ Memorizing/Memorizing/View/LoginSignUpView/LoginView.swift
@@ -130,5 +130,6 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+            .environmentObject(AuthStore())
     }
 }
