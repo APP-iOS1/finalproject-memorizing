5ceaa5cfda55b9e06d1aa2fdc4a8aec4098210fa
yeomsungpil <present7256@gmail.com>
2023-02-01 17:32:58 +0900

    feat: add market review and webview

---
+++ Memorizing/Memorizing/View/MyPageView/SafariWebView/WebView.swift
@@ -0,0 +1,20 @@
+//
+//  WebView.swift
+//  Memorizing
+//
+//  Created by 염성필 on 2023/02/01.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+       SafariView()
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView()
+    }
+}
