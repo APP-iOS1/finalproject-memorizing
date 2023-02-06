4ac2131eeef089182380f6985e4e7adae87c4edd
Jun <86897750+JKSeub@users.noreply.github.com>
2023-02-06 15:05:25 +0900

    [Feat] addlistrow layout 작업

--- Memorizing/Memorizing/View/WordNotesView/WordNoteAddView/AddListRow.swift
+++ Memorizing/Memorizing/View/WordNotesView/WordNoteAddView/AddListRow.swift
@@ -20,23 +20,25 @@ struct AddListRow: View {
             RoundedRectangle(cornerRadius: 10)
                 .stroke(Color.clear)
                 .backgroundStyle(Color.gray7)
-                .frame(width: 343, height: 100)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.1)
                 .overlay {
                     Button {
                         if let index = selectedWord.firstIndex(of: word.wordString ?? "no word name") {
                             selectedWord.remove(at: index)
                         }
                     } label: {
-                        VStack {
+                        VStack(spacing: 0) {
                             Text(word.wordString ?? "no word name")
                                 .font(.headline)
                                 .foregroundColor(.mainBlack)
-                            
-                            Spacer()
+                                .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.height * 0.05)
+                                .multilineTextAlignment(.center)
                             
                             Text(word.wordMeaning ?? "no word meaning")
                                 .font(.headline)
                                 .foregroundColor(.mainBlue)
+                                .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.height * 0.05)
+                                .multilineTextAlignment(.center)
                         }
                         .padding(.horizontal)
                         .padding(.vertical, 15)
