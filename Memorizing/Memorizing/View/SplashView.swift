b3db68e6c9f610d91a104c4a412d935904fb027f
HyeonGi Yoon <yhg920@naver.com>
2023-02-14 01:14:07 +0900

    [feat] 앱실행시 네트워크 상태 확인 추가

--- Memorizing/Memorizing/View/SplashView.swift
+++ Memorizing/Memorizing/View/SplashView.swift
@@ -10,8 +10,9 @@ import FirebaseAuth
 
 struct SplashView: View {
     @EnvironmentObject var authStore: AuthStore
-    @State private var isActive = false
-    
+    @State private var isActive: Bool = false
+    @State private var isNetworkError: Bool = false
+
     var body: some View {
         VStack {
             if isActive {
@@ -22,13 +23,31 @@ struct SplashView: View {
             }
         } // VStack
         .task {
-            if Auth.auth().currentUser != nil {
-                await authStore.signInDidExistingAuth()
+            if !(DeviceManager.shared.networkStatus) {
+                isNetworkError = true
+            } else {
+                if Auth.auth().currentUser != nil {
+                    await authStore.signInDidExistingAuth()
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    self.isActive = true
+                }
             }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                self.isActive = true
-            }
+            
         }
+        .customAlert(isPresented: $isNetworkError,
+                     title: "네트워크 상태 확인",
+                     message: "서버와의 통신이 원활하지 않습니다.\n \n이용에 불편을 드려 죄송합니다.\n잠시후에 다시 시도해 주세요",
+                     primaryButtonTitle: "확인",
+                     primaryAction: {
+                        UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            exit(0)
+                        }
+                     },
+                     withCancelButton: false,
+                     cancelButtonText: "")
     } // body
 } // struct
 
